1f3e93a700a60565c93ab619a8f6057cad5f3c3f
serp-ya <sergei.kundryukov1989@gmail.com>
2018-05-27 16:46:10 +0300

    Done

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+$x = (int) $_GET['x'];
+
+if (!$x) {
+    echo "Введите число x";
+    exit();
+}
+
+echo "Введённое число: $x", "<br />";
+
+
+$first = 1;
+$second = 1;
+
+while (true) {
+    if ($first > $x) {
+        echo "Задуманное число НЕ входит в числовой ряд";
+        exit();
+
+    } elseif ($first === $x) {
+        echo "Задуманное число входит в числовой ряд";
+        exit();
+    }
+
+    $third = $first;
+    $first += $second;
+    $second = $third;
+}
